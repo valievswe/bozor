7d43d074955387fd691d9cacce4b4f9671457c66
valievswe <barkamol.valiy@gmail.com>
2025-10-17 12:57:19 +0500

    req body log

--- src/server.js
+++ src/server.js
@@ -34,6 +34,8 @@ const { startScheduler } = require("./utils/cron");
 app.use(cors());
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Myrent API is running!",
